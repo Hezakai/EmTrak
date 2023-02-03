eafa59d6e8909d35c3f6a65c7b0354b061132b22
Eric Graves <graves.eric@gmail.com>
2023-02-02 18:33:32 -0600

    Added menu options and View Roles functionality

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ function mainQuestion() {
             type: 'list',
             name: 'mainQuestion',
             message: "What would you like to do?",
-            choices : ['View All', 'View Employees', 'View Departments', 'Quit']
+            choices : ['View Roles', 'View Employees', 'View Departments', 'Add Department','Add Role', 'Add Employee', 'Update Employee', 'Quit']
         }
     ]).then(answer => {
         switch (answer.mainQuestion) {
-            case 'View All':
-                viewAll()
+            case 'View Roles':
+                viewRoles()
                 break;
             case 'View Employees':
                 viewEmployees()
@@ -26,20 +26,42 @@ function mainQuestion() {
             case 'View Departments':
                 viewDeparments()
                 break;
+            case 'Add Department':
+                addDepartment()
+                break;
+            case 'Add Role':
+                addRole()
+                break;
+            case 'Add Employee':
+                addEmployee()
+                break;
+            case 'Update Employee':
+                updateEmployee()
+                break;
             default:
                 connection.end()
         }
     })
 }
 
-function viewAll() {
-    console.log('view all tables in a join')
+//    connection.query("SELECT favorite_books.book_name AS name, book_prices.price AS price FROM favorite_books JOIN book_prices ON favorite_books.book_price = book_prices.id")
 
+function viewRoles() {
+    console.log('view all tables in a join')
+    connection.query("SELECT role.title, role.salary, department.name FROM role JOIN department ON role.department_id = department.id", (err, data) => {
+        if (err) throw err;
+        console.log("")
+        console.table(data)
+    })
     mainQuestion()
 }
 
 function viewEmployees() {
     console.log('view only employees table');
+    connection.query("SELECT * FROM employee", (err, data) => {
+        if (err) throw err;
+        console.table(data)
+    })
     mainQuestion()
 }
 
@@ -50,3 +72,30 @@ function viewDeparments() {
     })
     mainQuestion()
 }
+
+
+function addDepartment() {
+    console.log('addDepartment')
+    mainQuestion()
+}
+
+
+function addRole() {
+    console.log('addRole')
+    mainQuestion()
+}
+
+
+function addEmployee() {
+    console.log('addEmployee')
+    mainQuestion()
+}
+
+
+function updateEmployee() {
+    console.log('updateEmployee')
+    //query emp id from user
+    mainQuestion()
+}
+
+
